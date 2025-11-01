6d68bc9d9e42e68deff82621f65f864a072180ac
Kingvic <oladimejivictor611@gmail.com>
2025-11-01 09:41:51 +0100

    updated transaction fields

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
-    sender: { type: String, required: true },
-    receiver: { type: String, required: true },
-    suiExness: { type: String, required: false},
-    amount: { type: Number, required: true },
+    sender: { type: String, required: false }, // Changed to false
+    receiver: { type: String, required: false }, // Changed to false
+    suiExness: { type: String, required: false },
+    voiceRecording: { type: [String], required: false },
+    emoji: { type: String, required: false },
+    message: { type: String, required: false },
+    amount: { type: Number, required: false }, // Changed to false
     token: { type: String, default: "SUI" },
-    txHash: { type: String, unique: true, required: false },
+    txHash: { type: String, unique: true, required: false }, // Changed to false
     status: {
         type: String,
         enum: ["pending", "completed", "failed"],
-        default: "pending"
+        default: "pending",
     },
     timestamp: { type: Date, default: Date.now },
 });
